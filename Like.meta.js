3f42c7936df12669750a47269cbb5a125acfce55
mrwaitergg <161702864+mrwaitergg@users.noreply.github.com>
2024-02-29 14:17:50 +0500

    Create Like.meta.js

---
+++ Like.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         YouTube Auto Like
+// @namespace    http://tampermonkey.net/
+// @version      1.20
+// @description  Automatically likes a video or livestream on YouTube
+// @author       Yukiteru
+// @match        https://www.youtube.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @require      https://greasyfork.org/scripts/470224-tampermonkey-config/code/Tampermonkey%20Config.js
+// @license      MIT
+// ==/UserScript==
